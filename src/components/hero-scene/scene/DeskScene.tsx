812b2f025adad2b451979e250ae65e4979cfbf1f
Arthur Bouchaud <art.bouchaud@gmail.com>
2026-03-27 17:38:20 +0100

    Agencement amélioré

--- src/components/hero-scene/scene/DeskScene.tsx
+++ src/components/hero-scene/scene/DeskScene.tsx
@@ -23,23 +23,23 @@ export default function DeskScene() {
   return (
     <group ref={groupRef} position={[0.72, -1.5, 0.14]}>
       <Rug position={[-0.6, 0, 0.58]} rotation={[0, 0, 0]} />
-      <Desk1 position={[-1, 0, -0.8]} rotation={[0, 0, 0]} />
-      <Desk2 position={[-1.8, 0, 0.7]} rotation={[0, 1.5, 0]} />
+      <Desk1 position={[-0.7, 0, -1.4]} rotation={[0, 0, 0]} />
+      <Desk2 position={[-1.3, 0, -0.1]} rotation={[0, 1.5, 0]} />
 
-      <EcranPrincipal position={[0, 0.9, -0.9]} rotation={[0, 0, 0]} />
-      <EcranSecondaire position={[-1.2, 0.9, -0.6]} rotation={[0, 0.6, 0]} />
+      <EcranPrincipal position={[0.5, 0.9, -1.3]} rotation={[0, 0, 0]} />
+      <EcranSecondaire position={[-0.8, 0.9, -1]} rotation={[0, 0.5, 0]} />
 
       <Keyboard position={[0, 0.90, -0.2]} rotation={[0, 0, 0]} />
       <Mouse position={[0.5, 0.90, -0.2]} rotation={[0.3, 3, 0]} />
       <Mug position={[1, 0.90, -0.1]} rotation={[0, 0, 0]} />
-      <PencilCup position={[-1.6, 0.90, 0.8]} rotation={[0, 0, 0]} />
+      <PencilCup position={[-1.2, 0.90, -0.6]} rotation={[0, 0, 0]} />
 
       <Chair position={[0.10, 0, 0.6]} rotation={[0, -2.7, 0]} />
 
-      <WallShelf position={[-0.7, 2.2, -1]} rotation={[0, 0, 0]} />
-      <WallBoard position={[0.8, 2.5, -1]} rotation={[0, 0, 0]} />
-      <WallClock position={[2.0, 2.28, -1]} />
-      <Plant position={[1.5, 0.1, -1]} rotation={[0, 0, 0]} />
+      <WallShelf position={[-1.3, 2.1, -1.3]} rotation={[0, 0, 0]} />
+      <WallBoard position={[0.2, 2.4, -1.3]} rotation={[0, 0, 0]} />
+      <WallClock position={[1.8, 2.0, -1.3]} />
+      <Plant position={[1.4, 0.1, -1.2]} rotation={[0, 0, 0]} />
     </group>
   );
 }
